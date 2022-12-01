f13c0a58f63712800dbb9b9f3d6cb7a3fd186df3
kresno.19 <kresno.19@gmail.com>
2022-12-01 08:50:30 +0700

    first commit

---
+++ application/views/template.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<!-- saved from url=(0037)https://simencrang.sukabumikab.go.id/ -->
+<html lang="zxx" class="no-js" style="display: block;"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="<?php echo base_url('assets/front/favicon.ico'); ?>">
+		<!-- Author Meta -->
+		<meta name="author" content="codepixer">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		
+		<!-- Site Title -->
+		<title>Siwardah</title>
+
+		<link href="<?php echo base_url('assets/front/'); ?>css" rel="stylesheet"> 
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="<?php echo base_url('assets/front/linearicons.css'); ?>">
+			<link rel="stylesheet" href="<?php echo base_url('assets/front/font-awesome.min.css'); ?>">
+			<link rel="stylesheet" href="<?php echo base_url('assets/front/bootstrap.css'); ?>">
+			<link rel="stylesheet" href="<?php echo base_url('assets/front/magnific-popup.css'); ?>">
+			<link rel="stylesheet" href="<?php echo base_url('assets/front/nice-select.css'); ?>">					
+			<link rel="stylesheet" href="<?php echo base_url('assets/front/animate.min.css'); ?>">
+			<link rel="stylesheet" href="<?php echo base_url('assets/front/owl.carousel.css'); ?>">
+			<link rel="stylesheet" href="<?php echo base_url('assets/front/main.css'); ?>">
+		<script type="text/javascript" charset="UTF-8" src="<?php echo base_url('assets/front/'); ?>common.js"></script><script type="text/javascript" charset="UTF-8" src="<?php echo base_url('assets/front/'); ?>util.js"></script></head>
+		<body style="display: block;">
+
+			  <header id="header" class="header-scrolled">
+			    <div class="container">
+			    	<div class="row align-items-center justify-content-between d-flex">
+				      <div id="logo">
+				        <a href="https://siwardah.garutkab.go.id/index.php"><img src="<?php echo base_url('assets/front/logo.png'); ?>" alt="" title=""></a>
+				      </div>		    		
+			    	</div>
+			    </div>
+			  </header><!-- #header -->
+
+
+			<!-- start banner Area -->
+			<section class="banner-area relative" id="home">
+				<div class="overlay overlay-bg"></div>	
+				<div class="container">
+					<div class="row fullscreen d-flex align-items-center justify-content-center" style="height: 601px;">
+						<div class="banner-content col-lg-8">
+							<h1 class="text-white">
+								Siwardah				
+							</h1>
+							<p class="pt-20 pb-20 text-white">
+								Mengawasi, Monitoring, dan Memudahkan Retribusi di Kabupaten Garut agar lebih terorganisir dan Transparant.
+							</p>
+						</div>											
+					</div>
+				</div>					
+			</section>
+			<!-- End banner Area -->	
+
+			<!-- Start feature Area -->
+			<section class="feature-area pb-100" id="service">
+				<div class="container">
+					<div class="row mockup-container">
+						<img class="mx-auto d-block img-fluid" src="<?php echo base_url('assets/front/'); ?>laptop.png" alt="">
+					</div>
+					<div class="row d-flex justify-content-center">
+						<div class="menu-content pt-100 pb-60 col-lg-10">
+							<div class="title text-center">
+								<h1 class="mb-10">Fitur Aplikasi yang Tersedia</h1>
+							</div>
+						</div>
+					</div>						
+					<div class="row">
+						<div class="col-lg-4">
+							<a href="http://siwardah.sukabumikab.go.id/login" style="color:inherit;">
+								<div class="single-feature">
+    								<span class="lnr lnr-database"></span>
+    								<h4>
+    									Input Data Retribusi
+    								</h4>
+    								<p>
+    									Fitur Aplikasi ini digunakan untuk Perangkat daerah yang memungut retribusi untuk masuk ke database retribusi.
+    								</p>
+							    </div>
+							</a>
+						</div>
+						<div class="col-lg-4">
+						    <a href="http://siwardah.garutkab.go.id/dokumen" style="color:inherit;">
+    							<div class="single-feature">
+    								<span class="lnr lnr-file-add"></span>
+    								<h4>
+    									Sinkronisasi Dokumen
+    								</h4>
+    								<p>
+    									Fitur Aplikasi ini digunakan untuk melakukan sinkronisasi dokumen retribusi yang ada di Kabupaten Garut.
+    								</p>
+    							</div>
+    						</a>
+						</div>
+						<div class="col-lg-4">
+						    <a href="http://siwardah.garutkab.go.id/publik" style="color:inherit;">
+    							<div class="single-feature">
+    								<span class="lnr lnr-smile"></span>
+    								<h4>
+    									Akses Publik
+    								</h4>
+    								<p>
+    									Fitur ini digunakan untuk masyarakat dapat mengetahui terkait progres retribusi yang ada di Kabupaten Garut.
+    								</p>
+    							</div>
+    						</a>
+						</div>												
+					</div>
+				</div>	
+			</section>
+			<!-- End feature Area -->
+			
+
+			<!-- start footer Area -->		
+			<footer class="footer-area section-gap-footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-5 col-md-6 col-sm-6">
+							<div class="single-footer-widget">
+								<h6>Siwardah</h6>
+								<p>
+									Sistem Informasi Pengawasan Retribusi Daerah
+								</p>
+								<p class="footer-text">
+									<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright ©<script>document.write(new Date().getFullYear());</script> All rights reserved <i class="fa fa-heart-o" aria-hidden="true"></i> Template by <a href="https://colorlib.com/" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+								</p>	
+							</div>
+						</div>												
+					</div>
+				</div>
+			</footer>	
+			<!-- End footer Area -->		
+
+			<script src="<?php echo base_url('assets/front/'); ?>jquery-2.2.4.min.js"></script>
+			<script src="<?php echo base_url('assets/front/'); ?>popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="<?php echo base_url('assets/front/'); ?>bootstrap.min.js"></script>			
+  			<script src="<?php echo base_url('assets/front/'); ?>easing.min.js"></script>			
+			<script src="<?php echo base_url('assets/front/'); ?>hoverIntent.js"></script>
+			<script src="<?php echo base_url('assets/front/'); ?>superfish.min.js"></script>	
+			<script src="<?php echo base_url('assets/front/'); ?>jquery.ajaxchimp.min.js"></script>
+			<script src="<?php echo base_url('assets/front/'); ?>jquery.magnific-popup.min.js"></script>	
+			<script src="<?php echo base_url('assets/front/'); ?>owl.carousel.min.js"></script>			
+			<script src="<?php echo base_url('assets/front/'); ?>jquery.sticky.js"></script>
+			<script src="<?php echo base_url('assets/front/'); ?>jquery.nice-select.min.js"></script>			
+			<script src="<?php echo base_url('assets/front/'); ?>parallax.min.js"></script>		
+			<script src="<?php echo base_url('assets/front/'); ?>mail-script.js"></script>	
+			<script src="<?php echo base_url('assets/front/'); ?>main.js"></script>	
+		
+	
+
+
+
+</body></html>
